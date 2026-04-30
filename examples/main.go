2c035bb54a1f4876a158beab6658907c78d8ddf2
罗杰 <xiaoyaojie005@gmail.com>
2026-04-30 22:19:21 +0800

    Add dynamic log level adjustment and new logger instance configuration examples

--- examples/main.go
+++ examples/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log/slog"
+
 	log "github.com/luojiego/slogx"
 )
 
@@ -28,4 +30,26 @@ func main() {
 	log.Info("Info level message")
 	log.Warn("Warning level message")
 	log.Error("Error level message")
+
+	// 演示 GetLevel / SetLevel 动态调整日志等级
+	currentLevel := log.GetLevel()
+	log.Info("Current log level", "level", currentLevel)
+
+	// 将日志等级提升为 INFO，DEBUG 级别日志将不再输出
+	log.SetLevel(slog.LevelInfo)
+	log.Info("Log level changed to INFO")
+	log.Debug("This debug message should NOT appear")
+	log.Info("This info message should still appear")
+
+	// 通过 Logger 实例也能获取和设置等级
+	logger2 := log.NewLogger(log.Config{
+		Level:    slog.LevelWarn,
+		Format:   "text",
+		Filename: "",
+		Stdout:   true,
+	})
+	log.Info("New logger level", "level", logger2.GetLevel())
+	logger2.SetLevel(slog.LevelError)
+	logger2.Warn("This warn message should NOT appear")
+	logger2.Error("This error message should still appear")
 }
